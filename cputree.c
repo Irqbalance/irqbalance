3df62ce0b46c1aaf3fb8922d48d4091a9c1cc1f1
Seiichi Ikarashi <s.ikarashi@jp.fujitsu.com>
2015-07-24 21:49:14 +0900

    irqbalance: add IRQ count into irq_info debug log
    IRQ count is missing in the current irq_info debug log.
    It needs to be printed for debugging.
    
    Also, it's better to print (uint64_t)(irq_info->load) as %llu, instead of %u.
    
    Signed-off-by: Seiichi Ikarashi <s.ikarashi@jp.fujitsu.com>
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- cputree.c
+++ cputree.c
@@ -345,8 +345,8 @@ static void dump_irq(struct irq_info *info, void *data)
 		indent[i] = log_indent[0];
 
 	indent[i] = '\0';
-	log(TO_CONSOLE, LOG_INFO, "%sInterrupt %i node_num is %d (%s/%u) \n", indent,
-	    info->irq, irq_numa_node(info)->number, classes[info->class], (unsigned int)info->load);
+	log(TO_CONSOLE, LOG_INFO, "%sInterrupt %i node_num is %d (%s/%llu:%llu) \n", indent,
+	    info->irq, irq_numa_node(info)->number, classes[info->class], info->load, (info->irq_count - info->last_irq_count));
 	free(indent);
 }
 
