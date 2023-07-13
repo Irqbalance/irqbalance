Irqbalance/irqbalance

commit 470a64b190628574c28a266bdcf8960291463191

activate_mapping: make sure to catch all errors
fprintf() is buffered and may not report an error which may be deferred
when fflush() is called (either explicitly or internally by fclose()).

Check for errors returned by fopen(), fprintf() and fclose() and add
IRQ_FLAG_AFFINITY_UNMANAGED accordingly.

Fixes: 55c5c321c73e ("arm64: Add irq aff change check For aarch64, ...")
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2184735
Signed-off-by: Robin Jarry <rjarry@redhat.com>

activate.c
@@ -75,16 +75,16 @@ static void activate_mapping(struct irq_info *info, void *data __attribute__((un
 	sprintf(buf, "/proc/irq/%i/smp_affinity", info->irq);
 	file = fopen(buf, "w");
 	if (!file)
-		return;
+		goto error;
 
 	cpumask_scnprintf(buf, PATH_MAX, applied_mask);
 	ret = fprintf(file, "%s", buf);
-	if (ret < 0) {
-		log(TO_ALL, LOG_WARNING, "cannot change IRQ %i affinity, will never try again\n", info->irq);
-		info->flags |= IRQ_FLAG_AFFINITY_UNMANAGED;
-	}
-	fclose(file);
+	if (fclose(file) || ret < 0)
+		goto error;
 	info->moved = 0; /*migration is done*/
+error:
+	log(TO_ALL, LOG_WARNING, "cannot change IRQ %i affinity, will never try again\n", info->irq);
+	info->flags |= IRQ_FLAG_AFFINITY_UNMANAGED;
 }
 
 void activate_mappings(void)

commit bc7794dc78474c463a26926749537f23abc4c082

activate_mapping: avoid logging error when there is none
Add missing return statement.

Fixes: 470a64b19062 ("activate_mapping: make sure to catch all errors")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

activate.c
@@ -89,6 +89,7 @@ static void activate_mapping(struct irq_info *info, void *data __attribute__((un
 	if (ret < 0)
 		goto error;
 	info->moved = 0; /*migration is done*/
+	return;
 error:
 	log(TO_ALL, LOG_WARNING,
 		"Cannot change IRQ %i affinity: %s\n",