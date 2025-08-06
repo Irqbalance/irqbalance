Irqbalance/irqbalance

commit d17bcc953c513f93553f531e5444553f2bf6ca46

Fix irqbalance cannot obtain the full name of irq
I find some irqs are not banned when I use --banmod. Because the irq desc name in AARCH64 contains space, for example:
 43:          1          0          0          0   ITS-MSI 16384 Edge      PCIe PME, aerdrv, pciehp
info->name is "pciehp", so check_for_module_ban returns 0 when I use "--bannmod=aerdrv"

procinterrupts.c
@@ -148,13 +148,13 @@ static void guess_arm_irq_hints(char *name, struct irq_info *info)
 void init_irq_class_and_type(char *savedline, struct irq_info *info, int irq)
 {
 	char *irq_name = NULL;
-	char *irq_mod = NULL;
 	char *savedptr = NULL;
 	char *last_token = NULL;
 	char *p = NULL;
-	int	is_xen_dyn = 0;
+	int is_xen_dyn = 0;
 #ifdef AARCH64
 	char *tmp = NULL;
+	char irq_fullname[PATH_MAX] = {0};
 #endif
 
 	irq_name = strtok_r(savedline, " ", &savedptr);
@@ -166,30 +166,40 @@ void init_irq_class_and_type(char *savedline, struct irq_info *info, int irq)
 		if (strstr(irq_name, "xen-dyn") != NULL)
 			is_xen_dyn = 1;
 		last_token = p;
+#ifdef AARCH64
+		/*
+		 * /proc/interrupts format defined, after of interrupt type
+		 * the reset string is mark the irq desc name.
+		 */
+		if (strncmp(irq_name, "Level", strlen("Level")) == 0 ||
+                                strncmp(irq_name, "Edge", strlen("Edge")) == 0)
+                        break;
+#endif
 	}
 
 #ifdef AARCH64
-		irq_name = last_token;
-		tmp = strchr(irq_name, '\n');
-		if (tmp)
-			*tmp = 0;
+	snprintf(irq_fullname, PATH_MAX, "%s %s", last_token, savedptr);
+	tmp = strchr(irq_fullname, '\n');
+	if (tmp)
+		*tmp = 0;
+#else
+	snprintf(irq_fullname, PATH_MAX, "%s", last_token);
 #endif
-	irq_mod = last_token;
 	info->irq = irq;
 
-	if (strstr(irq_name, "-event") != NULL && is_xen_dyn == 1) {
+	if (strstr(irq_fullname, "-event") != NULL && is_xen_dyn == 1) {
 		info->type = IRQ_TYPE_VIRT_EVENT;
 		info->class = IRQ_VIRT_EVENT;
 	} else {
 #ifdef AARCH64
-		guess_arm_irq_hints(irq_name, info);			
+		guess_arm_irq_hints(irq_fullname, info);			
 #else
 		info->type = IRQ_TYPE_LEGACY;
 		info->class = IRQ_OTHER;
 #endif
 	}
 	info->numa_node = get_numa_node(0);
-	info->name = strdup(irq_mod);
+	info->name = strdup(irq_fullname);
 }
 
 GList* collect_full_irq_list()

commit 4f53f8c8e6740ffb4097722f705586aee2ed35de

Fix irqbalance for Xen virtual event interrupts
Xen VIF interrupts were getting classified as other, instead of
virt-event:
```
 312:    2453008          0          0          0          0          0          0          0          0          0          0          0          0          0          0          0  xen-dyn-lateeoi     -event     vif13.0-q1-tx
Interrupt 312 node_num is -1 (other/0:7)
```

And then they were not being moved at all, and some interrupts were
running on CPU0 (which is also busy with other legacy interrupts).

In the past they were classified as virt-event:
```
Interrupt 343 node_num is -1 (virt-event/0)
```

This is a regression from irqbalance 1.0.9.

GDB shows that irq_fullname is 'vif1.0-q0-tx', and not '-event', so this
condition no longer triggers:
```
   if (strstr(irq_fullname, "-event") != NULL && is_xen_dyn == 1) {
        info->type = IRQ_TYPE_VIRT_EVENT;
        info->class = IRQ_VIRT_EVENT;
```

Instead `irq_name` should be used, which points to the previous token, '-event'.
After this patch the IRQ gets classified correctly (the number changed
because I rebooted inbetween):
```
 Interrupt 302 node_num is -1 (virt-event/0:0)
```

The regression was introduced by a change to ARM-specific code that
changed Xen code (accidentally?).
The commit was for lines of this form:
```
43:          1          0          0          0   ITS-MSI 16384 Edge      PCIe PME, aerdrv, pciehp
```
That should be unaffected by this change: it doesn't contain '-event'
and doesn't contain 'xen-dyn', so the 'if' condition will stay false
after this patch, and the 'else' side would run, which contains the ARM
code.

Fixes: d17bcc9 ("Fix irqbalance cannot obtain the full name of irq")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

procinterrupts.c
@@ -191,7 +191,7 @@ void init_irq_class_and_type(char *savedline, struct irq_info *info, int irq)
 #endif
 	info->irq = irq;
 
-	if (strstr(irq_fullname, "-event") != NULL && is_xen_dyn == 1) {
+	if (strstr(irq_name, "-event") != NULL && is_xen_dyn == 1) {
 		info->type = IRQ_TYPE_VIRT_EVENT;
 		info->class = IRQ_VIRT_EVENT;
 	} else {